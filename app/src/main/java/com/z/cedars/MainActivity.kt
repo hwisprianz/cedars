70c12694396b2d691e2b83cf441d82f484830de3
blate <blatez@qq.com>
2023-11-15 15:22:46 +0800

    create project

---
+++ app/src/main/java/com/z/cedars/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.z.cedars
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
